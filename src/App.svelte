bcd8d684eb7f692adb5c28e868dcc21b681301b3
huynt204567 <uchitoyuki@gmail.com>
2024-08-22 22:18:21 +0700

    no message

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
     // class Plural {
     //     name: String;
     //     untranslatable: String;
-    //     list: Array<PluralItem> = new Array();
+    //     item: Array<PluralItem> = new Array();
     // }
     //plural2
     class Plural2 {
@@ -22,11 +22,17 @@
         text: String;
     }
 
+    enum TableType {
+        String,
+        StringArray,
+        Plural,
+    }
+
     //string array 1
     // class StringArray {
     //     name: String;
     //     untranslatable: String;
-    //     list: Array<String> = new Array<String>();
+    //     item: Array<String> = new Array<String>();
     // }
     //string array 2
     class StringArray2 {
@@ -39,28 +45,76 @@
     let fileList: Array<File> = new Array();
 
     let jsonObj;
+
     let str;
     let strT;
-    let strL;
+    let strL: Array<String> = new Array();
+    let strSettings = {
+        colHeaders: ["Key", "Untranslatable", "Value"],
+        dataSchema: { name: null, untranslatable: null, text: null },
+        rowHeaders: true,
+        height: "auto",
+        autoWrapRow: true,
+        autoWrapCol: true,
+        with: "auto",
+        minSpareRows: 1,
+    };
 
     let stra;
     let straT;
     let straL: Array<StringArray2> = new Array<StringArray2>();
+    let straSettings = {
+        colHeaders: ["Key", "Untranslatable", "Value"],
+        dataSchema: { name: null, untranslatable: null, text: null },
+        rowHeaders: true,
+        height: "auto",
+        autoWrapRow: true,
+        autoWrapCol: true,
+        with: "auto",
+        minSpareRows: 1,
+    };
 
     let plur;
     let plurT;
     let plurL: Array<Plural2> = new Array<Plural2>();
+    let plurSettings = {
+        colHeaders: ["Key", "Untranslatable", "Quantity", "Value"],
+        dataSchema: {
+            name: null,
+            untranslatable: null,
+            quantity: null,
+            text: null,
+        },
+        rowHeaders: true,
+        height: "auto",
+        with: "auto",
+        minSpareRows: 1,
+        autoWrapRow: true,
+        autoWrapCol: true,
+    };
 
-    function createTable(datas, id) {
+    function loadTable(datas, id, table, type) {
+        if (table) {
+            loadData(table, datas);
+            return table;
+        }
         let container = document.getElementById(id);
-        container.innerHTML = "";
+        let setting;
+        switch (type) {
+            case TableType.String:
+                setting = strSettings;
+                break;
+            case TableType.Plural:
+                setting = plurSettings;
+                break;
+            case TableType.StringArray:
+                setting = straSettings;
+                break;
+        }
+
         let hot = new Handsontable(container, {
             data: datas,
-            rowHeaders: true,
-            colHeaders: true,
-            height: "auto",
-            autoWrapRow: true,
-            autoWrapCol: true,
+            ...setting,
             licenseKey: "non-commercial-and-evaluation", // for non-commercial use only
         });
         //neu map lai thi can tham chieu den jsonObj.resources
@@ -83,7 +137,7 @@
         return hot;
     }
 
-    function load(hot, data) {
+    function loadData(hot, data) {
         hot.loadData(data);
     }
 
@@ -93,10 +147,10 @@
             return x;
         }
 
-        if (arr.length == 1) {
-            x = [...x, arr];
+        if (!arr.length) {
+            x = [arr, ...x];
         } else {
-            x = [...arr];
+            return arr;
         }
         return x;
     }
@@ -108,6 +162,8 @@
     //de data phu hop voi bang
     function maping() {
         //str
+        str = convertToArray(str);
+        strL.splice(0, strL.length);
         strL = str?.map((item) => ({
             name: item["@_name"],
             untranslatable: item["@_untranslatable"] || "",
@@ -115,7 +171,8 @@
         }));
 
         //stra
-        straL.slice(0, straL.length - 1);
+        stra = convertToArray(stra);
+        straL.splice(0, straL.length);
         if (stra)
             stra.forEach((e) => {
                 e["item"].forEach((element) => {
@@ -128,7 +185,8 @@
             });
 
         //plural
-        plurL.slice(0, straL.length - 1);
+        plur = convertToArray(plur);
+        plurL.splice(0, straL.length);
         if (plur)
             plur.forEach((e) => {
                 e["item"].forEach((element) => {
@@ -167,36 +225,36 @@
         reader.onload = function (e) {
             const xmlContent = e.target.result;
             jsonObj = parser.parse(xmlContent);
-            str = jsonObj.resources["string"];
-            stra = jsonObj.resources["string-array"];
-            plur = jsonObj.resources["plurals"];
+            let str = jsonObj.resources["string"];
+            let stra = jsonObj.resources["string-array"];
+            let plur = jsonObj.resources["plurals"];
             maping();
-            strT = createTable(strL, "example");
-            plurT = createTable(plurL, "example1");
-            straT = createTable(straL, "example2");
+            strT = loadTable(strL, "stringT", strT, TableType.String);
+            plurT = loadTable(plurL, "pluralT", plurT, TableType.Plural);
+            straT = loadTable(
+                straL,
+                "stringarrayT",
+                straT,
+                TableType.StringArray,
+            );
+            console.log(jsonObj);
         };
         reader.readAsText(file);
-    }
-
-    function show() {
-        //console.log(jsonObj.resources);
-
-        // console.log(str);
-        // console.log(strL);
-
-        //console.log(stra);
-        //console.log(straL);
-
-        console.log(plur);
-        console.log(plurL);
-
-        //console.log(ori);
     }
 
     const builder = new XMLBuilder(options);
     let ori = null;
     function JSONtoXML() {
         ori = builder.build(jsonObj);
+    }
+
+    function openTab(evt, type) {
+        let i, tabcontent;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        document.getElementById(type).style.display = "block";
     }
 </script>
 
@@ -206,36 +264,38 @@
 	background-color:gray; padding:25px 0px;"
     >
         <input
+            on:change={OnUploadFile}
             id="upload"
             type="file"
             style="display:none;"
-            on:change={OnUploadFile}
         />
         <label for="upload" class="but">Upload</label>
     </div>
-    {#each fileList as file, i}
-        <button on:click={() => XMLtoJSON(file)}>{file.name}</button>
-        <!-- <a
-                style="padding:0px 2px; width: 30%;"
-                href={fileURL[i].toString()}
-                download="string.xml"
-            >
-                download
-            </a> -->
-    {/each}
+    <div>
+        <button on:click={(event) => openTab(event, "string")} id="defaultOpen"
+            >String</button
+        >
+        <button on:click={(event) => openTab(event, "plural")}>Plural</button>
+        <button on:click={(event) => openTab(event, "stringarray")}
+            >String-Array</button
+        >
+    </div>
 </div>
 
 <div class="content">
-    <h2>Table</h2>
-    <button on:click={show}>Show</button>
-    <div class="ta">
-        <div id="example"></div>
+    <div id="string" class="tabcontent">
+        <h2>String</h2>
+        <div id="stringT"></div>
     </div>
-    <div class="ta">
-        <div id="example1"></div>
+
+    <div id="plural" class="tabcontent">
+        <h2>Plural</h2>
+        <div id="pluralT"></div>
     </div>
-    <div class="ta">
-        <div id="example2"></div>
+
+    <div id="stringarray" class="tabcontent">
+        <h2>String-Array</h2>
+        <div id="stringarrayT"></div>
     </div>
 </div>
 
@@ -260,13 +320,17 @@
         padding: 16px;
         text-decoration: none;
         display: block;
+        width: 100%;
     }
 
-    .sidenav a:hover {
+    .sidenav button:hover {
         background-color: #ddd;
         color: black;
     }
-
+    .sidenav button:active {
+        background-color: #924040;
+        color: black;
+    }
     .content {
         margin-left: 200px;
         padding-left: 20px;
@@ -292,7 +356,7 @@
         border: 2px solid white;
     }
 
-    .ta {
-        padding: 2px;
+    .tabcontent {
+        display: none;
     }
 </style>
